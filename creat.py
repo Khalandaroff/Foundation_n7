b5c3a1adbeb2895c66c7fb10c26ace982706d4c1
Khalandaroff <khalandaroff@gmail.com>
2023-01-07 20:45:22 +0500

    Something

---
+++ creat.py
@@ -0,0 +1,7 @@
+# git init ----------> Git ni initilizatsiya qiladi
+# git add . ----------> staging array ga oladi va kuzatiladi
+# git commit -m "Komment" ----------> commit qilish
+# git commit -am "Komment" ----------> commit va add qilish
+# git restore . ----------> o'chib ketkan fayl or codlani qaytaradi
+# git log ----------> hamma qilingan commit qilingan
+# git checkout "fayl nomi" ----------> fayllarni qaytaradi
